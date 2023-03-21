5b6830fcf8546e670f5e88490b526b234bac8764
eys-man <kapuec2010@gmail.com>
2023-03-21 14:04:58 +0300

    feat: add change value searchbar test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import { describe, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import Card from './components/Card/card';
 import Search from './components/search/search';
 
@@ -11,7 +12,7 @@ describe('Card', () => {
     description: 'Description',
   };
 
-  test('Renders a card', () => {
+  test('Render a card', () => {
     // eslint-disable-next-line react/jsx-props-no-spreading
     render(<Card {...props} />);
     expect(screen.getByText(/Title/)).toBeInTheDocument();
@@ -19,7 +20,14 @@ describe('Card', () => {
   });
 });
 
-test('Change a searchBar', () => {
+test('Render SearchBar', () => {
   render(<Search />);
   expect(screen.getByRole('textbox')).toBeInTheDocument();
+});
+
+test('Change value of SearchBar', () => {
+  render(<Search />);
+  fireEvent.change(screen.getByRole<HTMLInputElement>('textbox'), { target: { value: '2023' } });
+  // userEvent.type(screen.getByRole<HTMLInputElement>('textbox'), '1');
+  expect(screen.getByRole<HTMLInputElement>('textbox').value).toBe('2023');
 });
