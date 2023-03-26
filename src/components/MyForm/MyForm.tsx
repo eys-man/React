7a98950d3c4205a2d7539925a4390716109701ea
eys-man <kapuec2010@gmail.com>
2023-03-26 19:12:19 +0300

    feat: add handling of reset and submit events

--- src/components/MyForm/MyForm.tsx
+++ src/components/MyForm/MyForm.tsx
@@ -9,25 +9,44 @@ class MyForm extends React.Component<Props> {
   cityInput: React.RefObject<HTMLSelectElement>;
   agreeInput: React.RefObject<HTMLInputElement>;
   genderMaleInput: React.RefObject<HTMLInputElement>;
-  genderFemaleInput: React.RefObject<HTMLInputElement>;
   fileInput: React.RefObject<HTMLInputElement>;
   constructor(props: Props) {
     super(props);
+    this.state = { isSubmit: false };
     this.nameInput = React.createRef<HTMLInputElement>();
     this.timeInput = React.createRef<HTMLInputElement>();
     this.cityInput = React.createRef<HTMLSelectElement>();
     this.agreeInput = React.createRef<HTMLInputElement>();
     this.genderMaleInput = React.createRef<HTMLInputElement>();
-    this.genderFemaleInput = React.createRef<HTMLInputElement>();
     this.fileInput = React.createRef<HTMLInputElement>();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event: { preventDefault: () => void }) {
+    event.preventDefault();
+
+    const name = (this.nameInput.current as HTMLInputElement).value;
+    const time = (this.timeInput.current as HTMLInputElement).value;
+    const city = (this.cityInput.current as HTMLSelectElement).value;
+    const isAgree = (this.agreeInput.current as HTMLInputElement).checked;
+    let gender = '';
+    if ((this.genderMaleInput.current as HTMLInputElement).checked === true)
+      gender = 'male';
+    else gender = 'female';
+    const fileName = (this.fileInput.current as HTMLInputElement).value;
+
+    alert(
+      `name = ${name}, birthday = ${time}, city = ${city}, isAgree = ${isAgree}, gender = ${gender}, fileName = ${fileName}`
+    );
   }
 
   render(): JSX.Element {
     return (
-      <div className="form">
+      <form className="form" onSubmit={this.handleSubmit}>
         <div>
           <label htmlFor="name">Name: </label>
-          <input id="name" type="text" ref={this.nameInput} />
+          <input id="name" type="text" defaultValue="" ref={this.nameInput} />
         </div>
         <div>
           <label htmlFor="birthdate">Birthdate: </label>
@@ -38,11 +57,17 @@ class MyForm extends React.Component<Props> {
             min="1900-01-01"
             max="2023-01-01"
             ref={this.timeInput}
+            defaultValue="2000-01-01"
           />
         </div>
         <div>
           <label htmlFor="city">Place of residence:</label>
-          <select name="city" id="city" ref={this.cityInput}>
+          <select
+            name="city"
+            id="city"
+            defaultValue="Mogilev"
+            ref={this.cityInput}
+          >
             <option value="Minsk">Minsk</option>
             <option value="Mogilev">Mogilev</option>
             <option value="Brest">Brest</option>
@@ -72,13 +97,7 @@ class MyForm extends React.Component<Props> {
               ref={this.genderMaleInput}
             />
             <label htmlFor="male">male</label>
-            <input
-              type="radio"
-              id="female"
-              name="gender"
-              value="female"
-              ref={this.genderFemaleInput}
-            />
+            <input type="radio" id="female" name="gender" value="female" />
             <label htmlFor="female">female</label>
           </div>
         </div>
@@ -87,9 +106,14 @@ class MyForm extends React.Component<Props> {
           <input type="file" id="avatar" name="file" ref={this.fileInput} />
         </div>
         <div>
-          <button>Submit</button>
+          <button type="submit" value="submit">
+            Submit
+          </button>
+          <button type="reset" value="reset">
+            Reset
+          </button>
         </div>
-      </div>
+      </form>
     );
   }
 }
