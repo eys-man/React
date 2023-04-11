1ac19629f080e9a86a84f1855c31f1d1da37ee50
eys-man <112211574+eys-man@users.noreply.github.com>
2023-04-11 09:48:55 +0300

    Update and rename test to Search.tsx

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './Search.css';
+
+const Search = () => {
+  const [search, setSearch] = useState(localStorage.getItem('search') || '');
+
+  const searchRef = useRef<string>();
+
+  useEffect((): void => {
+    searchRef.current = search;
+  }, [search]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('search', searchRef.current as string);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="search-wrapper">
+        <input
+          className="search"
+          type="text"
+          value={search}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
