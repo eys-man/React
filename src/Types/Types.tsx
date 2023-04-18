d35819a8023da6ec5e2e7ceb3ed51b558b0962dd
eys-man <kapuec2010@gmail.com>
2023-04-19 00:15:41 +0300

    feat: add types for Rick-n-Morty API

--- src/Types/Types.tsx
+++ src/Types/Types.tsx
@@ -37,6 +37,37 @@ export type NewCardData = {
   };
 };
 
+export type RMCardData = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+
+export type RMCardsData = {
+  info: {
+    count: number;
+    pages?: number;
+    next?: string;
+    prev?: string;
+  };
+  results: RMCardData[];
+};
+
 export type CardsData = {
   data: CardData[];
   info: {
