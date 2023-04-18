369e0fce0eb1cc1ca36f77c27f1d62d1ed387700
eys-man <kapuec2010@gmail.com>
2023-04-18 13:02:35 +0300

    fix: devtools error for form submit

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -8,6 +8,10 @@ export const store = configureStore({
     searchReducer,
     formReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
